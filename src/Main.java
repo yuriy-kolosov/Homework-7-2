283d8cfff49761789c1cfc9790f0e636ed2ced75
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-09-26 21:35:51 +0300

    Домашнее задание 7-2 (8-2) "Техническое собеседование, часть 2" - Билет 5 - Задача - Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание 7-2 (8-2) \"Техническое собеседование, часть 2\" - Билет 5 - Задача");
+        System.out.println();
+//        Напишите пример перехвата и обработки исключения в блоке try-catch метода
+
+    }
+
+}
